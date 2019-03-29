2ec38822cd57822eb976cd7f3692246d25ddef24
aleksspeaker <aleksspeaker@gmail.com>
2019-03-29 10:47:59 +0200

    Fixed setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // TODO: https://blog.sourcerer.io/webpack-101-empower-your-web-projects-with-modern-javascript-149f20d80812
 console.log('hello');
+document.querySelector('p').innerText = 'haha'
+var h1 = document.createElement('h1');
+h1.innerText = 'hahaha!'
+document.body.appendChild(h1);
